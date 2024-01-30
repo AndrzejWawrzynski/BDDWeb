a394d1a9bd6fd9c24b1201d5c17c50ecf8d10d6f
Andrzej Wawrzyński <wandrzej@interia.pl>
2024-01-30 15:43:51 +0100

    30.01.16.00

--- src/test/java/StepDefinitions/LoginSteps.java
+++ src/test/java/StepDefinitions/LoginSteps.java
@@ -3,19 +3,31 @@ package StepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginSteps {
+    WebDriver driver;
     @Given("Uzytkownik musi byc na stronie logowania")
     public void UzytkownikJestNaStronieLogowania(){
-        System.out.println("Uzytkownik jest na stronie logowania");
+        //System.setProperty("webdriver.chrome.driver", "//users//andy//chromedriver");
+        driver = new ChromeDriver();
+        driver.navigate().to("https://the-internet.herokuapp.com/login");
+        //System.out.println("Uzytkownik jest na stronie logowania");
     }
     @When("Uzytkownik wpisuje poprawny login")
     public void UzytkownikWpisujePoprawnyLogin() {
-        System.out.println("Uzytkownik wpisuje poprawny login");
+        WebElement inputUsername = driver.findElement(By.id("username"));
+        inputUsername.sendKeys("tomsmith");
+        //System.out.println("Uzytkownik wpisuje poprawny login");
     }
     @When("Uzytkownik wpisuje poprawne haslo")
     public void UzytkownikWpisujePoprawneHaslo() {
-        System.out.println("Uzytkownik wpisuje poprawne haslo");
+        WebElement inputPassword = driver.findElement(By.id("password"));
+        inputPassword.sendKeys("SuperSecretPassword!");
+        //System.out.println("Uzytkownik wpisuje poprawne haslo");
     }
     @When("Uzytkownik klika przycisk Login")
     public void UzytkownikKlikaPrzyciskLogin() {
